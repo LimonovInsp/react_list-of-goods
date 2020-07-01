94f486c77da3e81c6586935f6068688c63da8445
LimonovInsp <bis.limonov@gmail.com>
2020-07-01 14:31:00 +0300

    Updates

---
+++ precache-manifest.b2a6ea01eb3b19936f516c86f622576c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13a9e7d4716eeda503d988475b96d98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "959bdf7dc786e4965a3f",
+    "url": "./static/css/main.d06b7e79.chunk.css"
+  },
+  {
+    "revision": "3995907ca2736347f1b8",
+    "url": "./static/js/2.497e4aa0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.497e4aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "959bdf7dc786e4965a3f",
+    "url": "./static/js/main.21975c6e.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
